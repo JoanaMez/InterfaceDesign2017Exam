9494368caa77e7d3704acdeda36f504f424b7dba
ashtrus <zygimantas.butas@gmail.com>
2017-05-19 20:40:22 +0200

    pages split into small code blocks

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,9 +3,16 @@
   'use strict';
 
   $(function () {
-    // FastShell
 
-
+    // index.html
+    $(function () { $("#site-header").load("components/header.html"); });
+    $(function () { $("#intro-page").load("components/intro-page.html"); });
+    $(function () { $("#banner").load("components/banner.html"); });
+    $(function () { $("#about").load("components/about.html"); });
+    $(function () { $("#speakers").load("components/speakers.html"); });
+    $(function () { $("#tickets").load("components/tickets.html"); });
+    $(function () { $("#sponsors").load("components/sponsors.html"); });
+    $(function () { $("#footer").load("components/footer.html"); });
 
 
   });
