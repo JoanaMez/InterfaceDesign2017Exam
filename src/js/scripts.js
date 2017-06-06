ee0c7b7064bac53fe1687cbf0e110a26385b3d15
Joana <m.joana2@gmail.com>
2017-06-07 01:39:41 +0200

    events done, buttons on click doen

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -183,6 +183,16 @@
       });
 
       $( "#accordion" ).accordion();
+
+      $("#accordion img").click(function () {
+        swal({
+          title: "Sweet!",
+          text: "mpor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua." +
+          " At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, At accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et nonumy sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum. sanctus sea sed takimata u",
+          imageUrl: "assets/img/39507.jpg"
+        });
+
+      })
     });
 
   });
